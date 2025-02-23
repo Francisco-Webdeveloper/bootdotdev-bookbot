6b8524ed9542de64ff443d2afabffe3785db4c3e
Francisco Santos <francisco.santos@joby.aero>
2025-02-23 10:55:36 +0100

    calculate the total number of words in the text file

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+def main():
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    return text
+
 def get_book_text(file_path):
     file_contents = ""
     with open(file_path) as file:
         file_contents = file.read()
     return file_contents
 
-def main():
-    book_path = "./books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
+def count_total_words():
+    text = main()
+    words_list = text.split()
+    number_of_words = len(words_list)
+    print(f"{number_of_words} words found in the document")
 
-main()
+count_total_words()
