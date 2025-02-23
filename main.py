7f522b35b381c2c7930793b34de2591d9edd28cb
Francisco Santos <francisco.santos@joby.aero>
2025-02-23 22:19:58 +0100

    print the report with total number of chars and sorted list with counts

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from stats import count_total_words, count_each_char
+from stats import count_total_words, count_each_char, chars_count_sorted_list
 
 def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
     number_of_words = count_total_words(text)
-    number_of_chars = count_each_char(text)
-    print(f"{number_of_words} words found in the document")
-    print(number_of_chars)
+    number_of_chars_dict = count_each_char(text)
+    list_of_chars = chars_count_sorted_list(number_of_chars_dict)
+
+    # HeADER
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+
+    # Word count section
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+
+    # Character count section
+    print("--------- Character Count -------")
+    for char_dict in list_of_chars:
+        for char in char_dict:
+            print(f"{char}: {char_dict[char]}")
+
+    # Footer
+    print("============= END ===============")
+
 
 def get_book_text(file_path):
     file_contents = ""
