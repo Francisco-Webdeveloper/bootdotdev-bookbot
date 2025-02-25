d603ff5e541ab7784f76bbfd36362ccce2fcd17a
Francisco Santos <francisco.santos@joby.aero>
2025-02-25 07:37:56 +0100

    adjust the code to accept system arguments with sys module

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import sys
 from stats import count_total_words, count_each_char, chars_count_sorted_list
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    cli_arguments = len(sys.argv)
+    if cli_arguments < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     number_of_words = count_total_words(text)
     number_of_chars_dict = count_each_char(text)
     list_of_chars = chars_count_sorted_list(number_of_chars_dict)
+    print_report(number_of_words, list_of_chars)
 
-    # HeADER
+def get_book_text(file_path):
+    file_contents = ""
+    with open(file_path) as file:
+        file_contents = file.read()
+    return file_contents
+
+def print_report(number_of_words, list_of_chars):
+    # Heder
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
 
@@ -23,12 +37,5 @@ def main():
 
     # Footer
     print("============= END ===============")
-
-
-def get_book_text(file_path):
-    file_contents = ""
-    with open(file_path) as file:
-        file_contents = file.read()
-    return file_contents
 
 main()
